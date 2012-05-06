f4a775a9c566fa5097488f45a4a64bd4b57a4a81
Markus Gebhard <markus.gebhard@web.de>
2012-05-06 10:17:22 +0200

    Adapted code and schema to 1 and 3 clamp variants

--- Impulsformer/Impulsformer.c
+++ Impulsformer/Impulsformer.c
@@ -2,30 +2,36 @@
 Impulsformer - device to transform a voltage delivered from a current clamp
 to an S0-output with 1000 pulses per kilo-Watt-hour
 
-Uses an Atmel ATtiny13 - Copyright 2012 Markus Gebhard
+Uses an Atmel ATtiny13(A)) - Copyright 2012 Markus Gebhard
 
 Default oscillator setting 9,6MHz with prescaling by 8 to 1,2MHz
 
-Use ADC3/PB3 as analog input
-Use PB2 as pulse output with optocoupler active low
+One clamp variant:
+Use ADC3/PB3 as analog input,
+Use PB2 as pulse output with optocoupler active low;
+Three clamp variant:
+Use ADC1,2,3/PB2,4,3 as analog inputs,
+Use PB1 as pulse output with optocoupler active low;
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 
 #ifndef F_CPU
 #define F_CPU 1200000	// 1,2MHz = 9,6MHz/8
 #endif
+
+#define THREE_CLAMPS 0	// if 1 then adapt to the three clamp variant
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -33,20 +39,29 @@ Use PB2 as pulse output with optocoupler active low
 
 #define GRID_V	230;	// Standard voltage of power grid in Germany
 #define CLAMP_A	50;		// Maximum current that the current clamp transforms
+#define N_KWH     1000	// Number of pulses per kWh
 
 #define PULSEPORT PORTB	// the port on which the pulses are given
-#define PULSEDDR  DDRB	// direction of pulso port
-#define PULSE_1   PB2	// first pulse output port (there might be more)
-#define N_KWH     1000	// Number of pulses per kWh
+#define PULSEDDR  DDRB	// direction of pulse port
+#if THREE_CLAMPS
+#define PULSE_1   PB1	// pulse output port in three clamp variant
+#else
+#define PULSE_1   PB2	// pulse output port in one clamp variant
+#endif
 
 // global variables
 uint32_t meterconst, pulseconst, energy;
-uint16_t lastADC;
+#if THREE_CLAMPS
+volatile uint16_t[3] lastADC;
+volatile uint8_t activeADC;	// the actually active ADC port for multiplexing
+#else
+volatile uint16_t lastADC;			// value of last measurement for integration
+#endif
 
 // initialize the Pulse output port
 void init_Output( void )
 {
-	// set output port(s) as output
+	// set output port as output
 	PULSEDDR |= (1<<PULSE_1);
 	// set level on output port high
 	PULSEPORT |= (1<<PULSE_1);
@@ -67,7 +82,12 @@ void init_ADC( void )
 	// start second conversion to initialize lastADC for proper "integration"
 	ADCSRA |= (1<<ADSC);
 	while (ADCSRA & (1<<ADSC));
+#if THREE_CLAMPS
+	activeADC = 3;
+	lastADC[activeADC-1] = ADC;
+#else	
 	lastADC = ADC;
+#endif	
 }
 
 // initialize timer for continuous AD conversions and defined time base
@@ -96,13 +116,24 @@ void send_pulse(unsigned char port)
 // Interrupt on timer compare match to measure and integrate power
 ISR(TIM0_COMPA_vect)
 {
-	uint16_t actADC;
+	uint16_t actADC;				// actually measured value
 	while (ADCSRA & (1<<ADSC));
 	actADC = ADC;
 	// discrete integration via Trapezoid Sums I(f)a|b ~ ((b-a)/2)*(f(a)+f(b))
+#if THREE_CLAMPS
+	energy += meterconst * (lastADC[activeADC-1] + actADC);
+	lastADC[activeADC-1] = actADC;
+#else	
 	energy += meterconst * (lastADC + actADC);
 	lastADC = actADC;
+#endif	
 	// start next conversion
+#if THREE_CLAMPS
+	activeADC++;
+	if (activeADC != 3) activeADC%3;
+	// measure next ADC input
+	ADMUX  = (1<<REFS0) | (activeADC<<MUX0);
+#endif	
 	ADCSRA |= (1<<ADSC);
 }
 
@@ -116,6 +147,10 @@ int main( void )
 	meterconst = GRID_V;
 	meterconst *= CLAMP_A;
 	meterconst /= 2048U;
+#if THREE_CLAMPS
+	// in the three clamp variant the ADC channels are measured each 3rd time
+	meterconst *= 3;
+#endif	
 	// set energy limit to send a pulse kWh -> 1000 W * 60 min * 60 sec * 1000 ms
 	pulseconst = 3600000000U; 	// kWh in Wms
 	pulseconst /= N_KWH;		// distributed to n impulses per kWh

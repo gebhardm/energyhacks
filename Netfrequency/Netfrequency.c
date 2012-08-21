da991de1abd4f22a5b61138057d59efda6ba4249
Markus Gebhard <markus.gebhard@web.de>
2012-08-21 19:04:08 +0200

    timing adaptation

--- Netfrequency/Netfrequency.c
+++ Netfrequency/Netfrequency.c
@@ -1,15 +1,15 @@
 /* Application to display the net frequency with 9 LEDs
-   on a ATtiny2313 running at 12MHz
-   (instead of an AT89C2051P as in German Elektor 01-2012,
-   pages 22 and following)
-   INT0 is used to couple the net frequency
-   LEDs on PORTB: PB7-0 and PORTD: PD6
-   red:  PB7
-   yellow: PB6,5
-   green: PB4,3,2
-   yellow: PB1,0
-   red:  PD6
-   
+on a ATtiny2313 running at 12MHz
+(instead of an AT89C2051P as in German Elektor 01-2012,
+pages 22 and following)
+INT0 is used to couple the net frequency
+LEDs on PORTB: PB7-0 and PORTD: PD6
+red:  PB7
+yellow: PB6,5
+green: PB4,3,2
+yellow: PB1,0
+red:  PD6
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -33,12 +33,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef RS232
 #define BAUD 19200              // RS232 speed for debugging purposes
 #endif
+#define DIRECT 0				// measure frequency without average calculation
 #define NET 50					// set Net default frequency
 #define DIVIDER 8				// Prescaler of Timers
-#define DELTA 30
-// derived values for frequency detection
-#define FACTOR (unsigned int) (F_CPU / DIVIDER / NET) // relevant count takts per Hz
-//#define DELTA2 DELTA / 2
+#define NETCNT (unsigned int) (F_CPU / DIVIDER / NET) // relevant count takts per Hz
 // define the LEDs display frequencies
 // example for 50Hz as in Europe:
 // Factor = 12.000.000 / 8 / 50 = 30.000 counts equal 50 Hz
@@ -60,84 +58,105 @@ void LED_off( void );
 void LED_toggle( unsigned char );
 /***************************************************************************/
 //Global variables for Interrupt computation
-volatile unsigned int  mean;		// mean Timer-value
-volatile unsigned long sum;
-volatile unsigned char cnt;
+volatile unsigned char lastLED = 0;
+volatile unsigned long sum = 0;
+volatile unsigned char cnt = 0;
 /***************************************************************************/
 //Interrupt Routines for computing INT0
 ISR(INT0_vect)
 {
-	unsigned int timer;
-	timer = TCNT1;
+	int led;
+#if DIRECT == 0
+	sum += TCNT1;
 	TCNT1 = 0;
-	sum += timer;
 	cnt++;
+	if (cnt == NET)
+	{
+		sum /= NET;
+		led = sum;
+		led -= NETCNT;
+		led /= 30;
+		led += 5;
+		LED_clr(lastLED);
+		LED_set(led);
+		lastLED = led;
+		sum = 0;
+		cnt = 0;
+	}
+#else
+	led = TCNT1;
+	led -= NETCNT;
+	led /= 30;
+	led += 5;
+	TCNT1 = 0;
+	LED_clr(lastLED);
+	LED_set(led);
+	lastLED = led;
+#endif
 }
 // check, if Timer overflows
 ISR(TIMER1_OVF_vect)
 {
-	LED_set(1); LED_set(9);
-	_delay_ms(5);
-    LED_clr(1); LED_clr(9);
+	TCNT1 = 0;
 }
 /***************************************************************************/
 #ifdef RS232
 //RS-232 Routines
 void init_USART( unsigned int bps )
 {
-  unsigned int baud;
-  baud = ( F_CPU / 16UL / bps ) - 1;       // compute Teiler
-  UBRRH = (unsigned char) (baud>>8);       // set Baudrate
-  UBRRL = (unsigned char) baud;
-  UCSRB = (1<<RXEN)|(1<<TXEN);             // RS232-Rx/Tx on
-  UCSRC = (3<<UCSZ0);                      // set Protocol 8N1
+	unsigned int baud;
+	baud = ( F_CPU / 16UL / bps ) - 1;       // compute Teiler
+	UBRRH = (unsigned char) (baud>>8);       // set Baudrate
+	UBRRL = (unsigned char) baud;
+	UCSRB = (1<<RXEN)|(1<<TXEN);             // RS232-Rx/Tx on
+	UCSRC = (3<<UCSZ0);                      // set Protocol 8N1
 }
 
 void send_USART( unsigned char c )
 {
-  while ( !( UCSRA & (1<<UDRE)) );  // wait until transfer buffer empty
-  UDR = c;                          // write char
+	while ( !( UCSRA & (1<<UDRE)) );  // wait until transfer buffer empty
+	UDR = c;                          // write char
 }
 
 void sendchars_USART( char *s )
 {
-  while ((*s != 0))
-  {                                 // output all chars
-     send_USART(*s++);
-  } 
+	while ((*s != 0))
+	{                                 // output all chars
+		send_USART(*s++);
+	} 
 }
 
 void send_uint( unsigned int n )
 {
-  unsigned char dig[5], num = 0, i; // compute 5 digit integer without sign
-  do
-  {
-    dig[num++] = ( n % 10 ) + 0x30;
-	n /= 10;
-  } while (n != 0);
-  for (i=num; i!=0; i--) send_USART(dig[i-1]);
+	unsigned char dig[5], num = 0, i; // compute 5 digit integer without sign
+	do
+	{
+		dig[num++] = ( n % 10 ) + 0x30;
+		n /= 10;
+	} while (n != 0);
+	for (i=num; i!=0; i--) send_USART(dig[i-1]);
 }
 #endif
 /***************************************************************************/
 // LED Routines
 void LED_init( void )
 {
-   DDRB = 0xff;
-   DDRD = (1<<PD6);
-   PORTB = 0xff;
-   PORTD = (1<<PD6);
+	DDRB = 0xff;
+	DDRD = (1<<PD6);
+	PORTB = 0xff;
+	PORTD = (1<<PD6);
 }
 // LED on
 void LED_set( unsigned char n )
 {
-  if (n<9) PORTB &= ~(1<<(8-n));
-  else if (n==9) PORTD &= ~(1<<PD6);
+	if ((n>0) && (n<9)) PORTB &= ~(1<<(8-n));
+	else if (n==9) PORTD &= ~(1<<PD6);
 }
 // LED off
 void LED_clr( unsigned char n )
 {
-  if (n<9) PORTB |= (1<<(8-n));
-  else if (n==9) PORTD |= (1<<PD6);
+	if ((n>0)&&(n<9)) PORTB |= (1<<(8-n));
+	else if (n==9) PORTD |= (1<<PD6);
 }
 void LED_off( void )
 {
@@ -145,59 +164,47 @@ void LED_off( void )
 }
 void LED_toggle( unsigned char n )
 {
-  if (n<9) PORTB ^= (1<<(8-n));
-  else if (n==9) PORTD ^= (1<<PD6);
+	if (n<9) PORTB ^= (1<<(8-n));
+	else if (n==9) PORTD ^= (1<<PD6);
 }
 /***************************************************************************/
 // main program
 int main( void )
 {
-  unsigned char i; 		     // counter variable
-  // Ausgabe vorbereiten
-  #ifdef RS232
-  init_USART( BAUD );
-  send_USART(27); sendchars_USART("[2J"); // clear screen
-  sendchars_USART("Net loupe");
-  #endif
-  // Net frequecy read preparation
-  MCUCR |= (0x02 << ISC00);  // Interrupt on falling edge of INT0
-  //MCUCR |= (0x03 << ISC00);  // Interrupt on rising edge on INT0
-  GIMSK |= (1 << INT0);   	 // release INT0
-  // Timer initialization
-  switch (DIVIDER)
-  {
-  	case 1: TCCR1B |= (0x01 << CS10); break; 
-	case 8: TCCR1B |= (0x02 << CS10); break; 
-    case 64: TCCR1B |= (0x03 << CS10); break;
-  }
-  TIMSK  |= (1 << TOIE1);  // Interrupt on timer overflow
-  // LEDs initialization
-  LED_init();
-  // LEDs count through (used just as a appetizer)
-  for (i=1;i<=9;i++)
-  {
-    LED_set(i);
-    _delay_ms(250);
-	LED_clr(i);
-  }
-  sum = 0;
-  cnt = 0;
-  // Frequency measuring preparation
-  sei();                    // activate Interrupt control
-  TCNT1 = 0;         		// Timer start
-  // program main loop - interrupt delivers mean value to display
-  while (1)
-  {
-  	if (cnt == NET)
+	unsigned char i; 		     // counter variable
+	// Ausgabe vorbereiten
+#ifdef RS232
+	init_USART( BAUD );
+	send_USART(27); sendchars_USART("[2J"); // clear screen
+	sendchars_USART("Net loupe");
+#endif
+	// Net frequecy read preparation
+	//MCUCR |= (0x02 << ISC00);  // Interrupt on falling edge of INT0
+	MCUCR |= (0x03 << ISC00);  // Interrupt on rising edge on INT0
+	GIMSK |= (1 << INT0);   	 // release INT0
+	// Timer initialization
+	switch (DIVIDER)
 	{
-		mean = (unsigned int) (sum / cnt);
-		cnt = 0; sum = 0;
-		#ifdef RS232
-		send_uint(mean); send_USART(0x0d); send_USART(0x0a);
-		#endif
-		LED_off();
-		LED_set((unsigned int)((mean - FACTOR)/DELTA)+5);
+	case 1: TCCR1B |= (0x01 << CS10); break; 
+	case 8: TCCR1B |= (0x02 << CS10); break; 
+	case 64: TCCR1B |= (0x03 << CS10); break;
 	}
-  }
-  return(0);
+	TIMSK  |= (1 << TOIE1);  // Interrupt on timer overflow
+	// LEDs initialization
+	LED_init();
+	// LEDs count through (used just as a appetizer)
+	for (i=1;i<=9;i++)
+	{
+		LED_set(i);
+		_delay_ms(250);
+		LED_clr(i);
+	}
+	// Frequency measuring preparation
+	sei();                    // activate Interrupt control
+	TCNT1 = 0;         		// Timer start
+	// program main loop - everything is done in the interrupt
+	while (1)
+	{
+	}
+	return(0);
 }

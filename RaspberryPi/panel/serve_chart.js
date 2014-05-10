c59f06c1bdad27bf7bdeac2eef47e28f90a12b48
Markus Gebhard <markus.gebhard@web.de>
2014-05-10 13:48:41 +0200

    getting db values

--- RaspberryPi/panel/serve_chart.js
+++ RaspberryPi/panel/serve_chart.js
@@ -10,10 +10,12 @@ uses
   socket.io module: http://github.com/learnboost/socket.io
 ************************************************************/
 var mysql = require('mysql');
-var http = require('http').createServer(handler);
+var http = require('http').createServer(handler).listen(8080);
 var fs = require('fs');
 var url = require('url');
 var path = require('path');
+var qs = require('querystring');
+var series = {};
 
 // prepare database connection
 var database = mysql.createConnection(
@@ -25,15 +27,6 @@ var database = mysql.createConnection(
    }
 );
 
-// connect to the database flm
-database.connect(function(err) {
-  if (err) throw err;
-  console.log('Database flm successfully connected');
-});
-
-// set up http server on port 8080
-http.listen(8080);
-
 // Serve the http request
 function handler (req, res) {
          var uri = url.parse(req.url).pathname;
@@ -44,39 +37,65 @@ function handler (req, res) {
             '.css': "text/css",
             '.js': "text/javascript"
          };
-
+// compute request method
+         if (req.method == 'POST') {
+            var content = '';
+            req.on('data', function (data) {
+               content += data;
+            });
+            req.on('end', function () {
+               var params = qs.parse(content);
+               var fromDateTime = params["fromDate"] + ' ' + params["fromTime"];
+               var toDateTime = params["toDate"] + ' ' + params["toTime"];
+               // connect to the database
+               database.connect(function(err) {
+                 if (err) throw err;
+                 console.log('Database flm connected');
+               });
+               // fetch data
+               var queryStr = 'SELECT * FROM flmdata WHERE timestamp >= \''
+                              + fromDateTime + '\' AND timestamp <= \''
+                              + toDateTime + '\';';
+               var query = database.query(queryStr, function(err, rows, fields) {
+                  if (err) throw err;
+                  for (var i in rows) {
+                    if (series[rows[i].sensor] == null) series[rows[i].sensor] = new Array();
+                    series[rows[i].sensor].push([rows[i].timestamp,rows[i].value]);                    
+                  }
+                 console.log(series);
+               });
+               // close the database
+               database.end(function(err){
+                 if (err) throw err;
+                 console.log('Database closed');
+               });
+            });
+         }
+// serve requested files
          fs.exists(filename, function(exists) {
            if(!exists) {
-             switch(req.url) {
-               case '/query':
-                 req.on('data', function(chunk) {
-                   console.log(chunk.toString());
-                 });
-               default:
-                 res.writeHead(404, {"Content-Type": "text/plain"});
-                 res.write("404 Not Found\n");
-                 res.end();
-                 return;
-                 break;
-              }
-            }
+             res.writeHead(404, {"Content-Type": "text/plain"});
+             res.write("404 Not Found\n");
+             res.end();
+             return;
+           }
 
-            if (fs.statSync(filename).isDirectory()) filename += '/chart.html';
+           if (fs.statSync(filename).isDirectory()) filename += '/chart.html';
 
-            fs.readFile(filename, "binary", function(err, file) {
-              if(err) {
+           fs.readFile(filename, "binary", function(err, file) {
+             if(err) {
                 res.writeHead(500, {"Content-Type": "text/plain"});
                 res.write(err + "\n");
                 res.end();
                 return;
-              }
+             }
 
-              var headers = {};
-              var contentType = contentTypesByExtension[path.extname(filename)];
-              if (contentType) headers["Content-Type"] = contentType;
-              res.writeHead(200, headers);
-              res.write(file, "binary");
+             var headers = {};
+             var contentType = contentTypesByExtension[path.extname(filename)];
+             if (contentType) headers["Content-Type"] = contentType;
+             res.writeHead(200, headers);
+             res.write(file, "binary");
               res.end();
-            });
+           });
          });
 };

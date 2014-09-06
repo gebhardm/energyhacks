cefa348440053f40bc6b7ce77cf15e34996e1b5f
Markus Gebhard <markus.gebhard@web.de>
2014-09-06 15:16:32 +0200

    LED flashes to show activity

--- AVRNetIOduino/AVRNetIO_MQTT_DS/AVRNetIO_MQTT_DS.ino
+++ AVRNetIOduino/AVRNetIO_MQTT_DS/AVRNetIO_MQTT_DS.ino
@@ -9,22 +9,21 @@
 
 OneWire  ds(28);  // on pin 28/ADC1
 
-const char hex[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+const char hex[16] = {
+  '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
-#define ledPin 1
+#define ledPin 1  // LED is connected at J11 cathode to ATmega, 10k replaced by 1k to +5V
 #define DEBUG 0
 
-//Global variables for Interrupt computation
-unsigned long sum = 0, Hz = 0, last = 0;
-byte cnt = 0;
 char payload[60];
-
-// The AVRNetIO's MAC as provided by Pollin
+/*************************************************************************/
+// The AVRNetIO's MAC as provided by Pollin - use your own!
 byte mac[]    = { 
   0x00, 0x22, 0xF9, 0x01, 0x2E, 0x49 };
-// The FLM's IP address
+// The FLM's IP address - replace with your broker's IP address
 byte flm[] = { 
   192, 168, 0, 50 };
+/*************************************************************************/
 
 void callback(char* topic, byte* payload, unsigned int length) {
   if (length>0) {
@@ -39,15 +38,21 @@ void setup()
 {
   // initialize serial interface
   if (DEBUG) Serial.begin(9600);
+  // enable LED
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH); // LED is active low  
   // initialize ethernet controller by DHCP
   if(Ethernet.begin(mac) == 0) {
     // something went wrong
     while(true)
     { 
-      digitalWrite(ledPin, HIGH);
-      delay(500);
-      digitalWrite(ledPin, LOW);
-      delay(500);        
+      for (int i=0;i<3;i++) {
+        digitalWrite(ledPin, LOW);
+        delay(100);
+        digitalWrite(ledPin, HIGH);
+        delay(100);        
+      }
+      delay(1000);
     }
   } 
   else {
@@ -56,6 +61,7 @@ void setup()
 }
 
 char* createPayload(long value, char* unit, byte dec)
+// <val>, <unit>, <decimal point position>
 {
   char val[10];
   int len, dp;
@@ -81,72 +87,83 @@ char* createPayload(long value, char* unit, byte dec)
   return payload;
 }
 
-void loop()
-{
-  // handling of temperature sensor(s)
+void flashLED( void ) {
+  digitalWrite(ledPin, LOW);
+  delay(20);
+  digitalWrite(ledPin, HIGH);
+}
+
+// handling of temperature sensor(s)
+void handle_ds18x20( void ) {
   byte i, present = 0, type_s, data[12], addr[8];
   char addrhex[17];
   char topic[32] = "";
+  if(!ds.search(addr)) {
+    ds.reset_search();
+    delay(250);
+  }
+  switch (addr[0]) {
+  case 0x10:
+    type_s = 1;
+    break;
+  case 0x28:
+    type_s = 0;
+    break;
+  case 0x22:
+    type_s = 0;
+    break;
+  default:
+    if (DEBUG) Serial.println("Device is not a DS18x20 family device.");
+    return;
+  }
+  // make address string
+  for (i=0;i<8;i++) {
+    addrhex[i*2] = hex[addr[i] >> 4];
+    addrhex[i*2+1] = hex[addr[i]&0xF];
+  }
+  addrhex[16] = 0x00;
+  strcat(topic,"/sensor/");
+  strcat(topic,addrhex);
+  strcat(topic,"/gauge");
+  if (DEBUG) Serial.println(topic);
   //
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion
+  delay(1000);
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xbe); // read scratchpad
+  for( i=0;i<9;i++) data[i] = ds.read();
+  // compute read sensor data
+  unsigned int raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // count remain gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } 
+  else {
+    byte cfg = (data[4] & 0x60);
+    if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
+    // default is 12 bit resolution, 750 ms conversion time
+  }
+  client.publish(topic,createPayload(raw*100/16,"°C",2));
+}
+
+// main loop
+void loop()
+{
   client.loop();
   if (client.connected()) {
-    if(!ds.search(addr)) {
-      ds.reset_search();
-      delay(250);
-    }
-    switch (addr[0]) {
-    case 0x10:
-      type_s = 1;
-      break;
-    case 0x28:
-      type_s = 0;
-      break;
-    case 0x22:
-      type_s = 0;
-      break;
-    default:
-      if (DEBUG) Serial.println("Device is not a DS18x20 family device.");
-      return;
-    }
-    // make address string
-    for (i=0;i<8;i++) {
-      addrhex[i*2] = hex[addr[i] >> 4];
-      addrhex[i*2+1] = hex[addr[i]&0xF];
-    }
-    addrhex[16] = 0x00;
-    strcat(topic,"/sensor/");
-    strcat(topic,addrhex);
-    strcat(topic,"/gauge");
-    if (DEBUG) Serial.println(topic);
-    //
-    ds.reset();
-    ds.select(addr);
-    ds.write(0x44,1); // start conversion
-    delay(1000);
-    present = ds.reset();
-    ds.select(addr);
-    ds.write(0xbe); // read scratchpad
-    for( i=0;i<9;i++) data[i] = ds.read();
-    // compute read sensor data
-    unsigned int raw = (data[1] << 8) | data[0];
-    if (type_s) {
-      raw = raw << 3; // 9 bit resolution default
-      if (data[7] == 0x10) {
-        // count remain gives full 12 bit resolution
-        raw = (raw & 0xFFF0) + 12 - data[6];
-      }
-    } 
-    else {
-      byte cfg = (data[4] & 0x60);
-      if (cfg == 0x00) raw = raw << 3;  // 9 bit resolution, 93.75 ms
-      else if (cfg == 0x20) raw = raw << 2; // 10 bit res, 187.5 ms
-      else if (cfg == 0x40) raw = raw << 1; // 11 bit res, 375 ms
-      // default is 12 bit resolution, 750 ms conversion time
-    }
-    client.publish(topic,createPayload(raw*100/16,"°C",2));
+    handle_ds18x20();
   }
   else {
     client.connect("arduino");
   }
+  flashLED();
   delay(1000);
 }

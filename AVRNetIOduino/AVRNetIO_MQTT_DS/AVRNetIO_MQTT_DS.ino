49192f46ea62badc64ed4bc64973011a9e9408ed
Markus Gebhard <markus.gebhard@web.de>
2014-09-18 19:19:16 +0200

    temperature publishing also with original Arduino Ethernet

--- AVRNetIOduino/AVRNetIO_MQTT_DS/AVRNetIO_MQTT_DS.ino
+++ AVRNetIOduino/AVRNetIO_MQTT_DS/AVRNetIO_MQTT_DS.ino
@@ -1,25 +1,45 @@
 /* AVRNetIO using MQTT to post sensor readings to the FLM broker */
+
+#define AVRNETIO 0
+#define DEBUG 0
+
+// use the SPI capability to connect the Ethernet shield
 #include <SPI.h>
-#include <UIPEthernet.h>  // https://github.com/ntruchsess/arduino_uip
-#include <PubSubClient.h>  // https://github.com/knolleary/pubsubclient
-#include <OneWire.h> // http://www.pjrc.com/teensy/td_libs_OneWire.html
+// load the correct Ethernet library for your Arduino/derivate
+//#include <UIPEthernet.h> // https://github.com/ntruchsess/arduino_uip - use with AVRNetIO
+#include <Ethernet.h> // use with Arduino Ethernet or Ethernet Shield
+// the MQTT publish/subscribe library
+#include <PubSubClient.h>   // https://github.com/knolleary/pubsubclient
+// the Dallas/Maxim one-wire library
+#include <OneWire.h>        // http://www.pjrc.com/teensy/td_libs_OneWire.html
 
 // The DallasTemperature library can do all this work for you!
 // http://milesburton.com/Dallas_Temperature_Control_Library
 
-OneWire  ds(28);  // on pin 28/ADC1
-
-const char hex[16] = {
+// for translating HEX to printout
+const char PROGMEM hex[16] = {
   '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
+// define the debug LED port
+#if AVRNETIO
 #define ledPin 1  // LED is connected at J11 cathode to ATmega, 10k replaced by 1k to +5V
-#define DEBUG 0
+#else
+#define ledPin 9 // LED on pin 9 of Arduino Ethernet
+#endif
 
 char payload[60];
 /*************************************************************************/
 // The AVRNetIO's MAC as provided by Pollin - use your own!
-byte mac[]    = { 
+#if AVRNETIO
+OneWire  ds(28);  // on pin 28/ADC1 on AVRNetIO
+byte mac[] = { 
   0x00, 0x22, 0xF9, 0x01, 0x2E, 0x49 };
+#else
+OneWire ds(8); // on pin 8 of Arduino Ethernet
+// the Arduino Ethernat MAC is written on the PCB backside - use your own!
+byte mac[] = {
+  0x90, 0xa2, 0xda, 0x0e, 0x50, 0x1b};  
+#endif  
 // The FLM's IP address - replace with your broker's IP address
 byte flm[] = { 
   192, 168, 0, 50 };
@@ -34,23 +54,40 @@ void callback(char* topic, byte* payload, unsigned int length) {
 EthernetClient ethClient;
 PubSubClient client(flm, 1883, callback, ethClient);
 
+// flash the LED to display a condition
+void flashLED( void ) {
+#ifdef AVRNETIO  
+  digitalWrite(ledPin, LOW);
+  delay(25);
+  digitalWrite(ledPin, HIGH);
+#else
+  digitalWrite(ledPin, HIGH);
+  delay(25);
+  digitalWrite(ledPin, LOW);
+#endif
+}
+
+// the setup routine to initialize everything needed
 void setup()
 {
-  // initialize serial interface
-  if (DEBUG) Serial.begin(9600);
+  // initialize serial interface if required for debugging purposes
+#if DEBUG
+  Serial.begin(9600);
+#endif
   // enable LED
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH); // LED is active low  
+#if AVRNETIO 
+  digitalWrite(ledPin, HIGH); // LED is active low on AVRNetIO
+#else
+  digitalWrite(ledPin, LOW);
+#endif 
   // initialize ethernet controller by DHCP
   if(Ethernet.begin(mac) == 0) {
     // something went wrong
     while(true)
     { 
       for (int i=0;i<3;i++) {
-        digitalWrite(ledPin, LOW);
-        delay(100);
-        digitalWrite(ledPin, HIGH);
-        delay(100);        
+        flashLED();        
       }
       delay(1000);
     }
@@ -60,6 +97,7 @@ void setup()
   };
 }
 
+// format the payload to be published
 char* createPayload(long value, char* unit, byte dec)
 // <val>, <unit>, <decimal point position>
 {
@@ -84,13 +122,10 @@ char* createPayload(long value, char* unit, byte dec)
   strcat(payload,",\"");
   strcat(payload,unit);
   strcat(payload,"\"]");
+#if DEBUG
+  Serial.println(payload);
+#endif
   return payload;
-}
-
-void flashLED( void ) {
-  digitalWrite(ledPin, LOW);
-  delay(20);
-  digitalWrite(ledPin, HIGH);
 }
 
 // handling of temperature sensor(s)
@@ -113,7 +148,9 @@ void handle_ds18x20( void ) {
     type_s = 0;
     break;
   default:
-    if (DEBUG) Serial.println("Device is not a DS18x20 family device.");
+#if DEBUG
+    Serial.println("Device is not a DS18x20 family device.");
+#endif
     return;
   }
   // make address string
@@ -125,7 +162,9 @@ void handle_ds18x20( void ) {
   strcat(topic,"/sensor/");
   strcat(topic,addrhex);
   strcat(topic,"/gauge");
-  if (DEBUG) Serial.println(topic);
+#if DEBUG
+  Serial.println(topic);
+#endif
   //
   ds.reset();
   ds.select(addr);

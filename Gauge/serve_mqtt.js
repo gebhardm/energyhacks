37aa2c92c1e2d8baf2e767d3362c629126e867e9
Markus Gebhard <markus.gebhard@web.de>
2015-04-10 13:57:37 +0200

    Get Gauge running on a windows PC
    Adaptations to current mqtt and socket.io modules; following installation steps performed:
    Install node.js v0.12.2
    npm install mqtt socket.io (be aware to have python installed)
    Install mosquitto 1.4.1 (incl. pthreads-2.9.1, OpenSSL, vc 2008 redist)
    
    run node serve_mqtt.js
    run mosquitto -v (to see traffic)
    run mosquitto_pub -h 127.0.0.1 -t /sensor/test/gauge -m [66,\"W\"] (escape the string quotes)

--- Gauge/serve_mqtt.js
+++ Gauge/serve_mqtt.js
@@ -2,14 +2,14 @@
  * This is script to serve a gauge display on received MQTT messages
  * topic: /sensor/<sensor_id>/gauge
  * payload: [<timestamp>, <value>, <unit>]
- * Markus Gebhard, Karlsruhe, 01/2015
+ * Markus Gebhard, Karlsruhe, 2015
  *
  * Released under the MIT license. See LICENSE file for details.
  *
  * Static http server part taken from Ryan Florence (rpflorence on github)
  * https://gist.github.com/rpflorence/701407
  * ************************************************************
- * Note: Use socket.io v1.0 for this script...
+ * Note: see package.json for dependencies
  */
 // use http for page serving, fs for getting the *.html files
 var httpport = 1080;
@@ -19,8 +19,8 @@ var mqtt = require("mqtt");
 
 var mqttclient;
 
-// specify here your MQTT broker's data
-var mqttbroker = "192.168.0.50", mqttport = "1883";
+// specify your MQTT broker's data here
+var mqttbroker = "localhost", mqttport = "1883";
 
 var http = require("http").createServer(httphandler).listen(httpport);
 
@@ -36,92 +36,108 @@ var io = require("socket.io")(http);
 var sensors = {};
 
 function mqttconnect() {
-    mqttclient = mqtt.connect({
-        port: mqttport,
-        host: mqttbroker
-    });
-    // handle socketio requests
-    io.on("connection", function(socket) {
-        // handle subscription request(s)
-        socket.on("subscribe", function(data) {
-            mqttclient.subscribe(data.topic);
-        });
-    });
-    // handle mqtt messages
-    mqttclient.on("message", function(topic, payload) {
-        var topicArray = topic.split("/");
-        switch (topicArray[1]) {
-          case "sensor":
-            handle_sensor(topicArray, payload);
-            break;
+	mqttclient = mqtt.connect({
+			port : mqttport,
+			host : mqttbroker
+		});
+	// handle socketio requests
+	io.on("connection", function (socket) {
+		// handle subscription request(s)
+		socket.on("subscribe", function (data) {
+			mqttclient.subscribe(data.topic);
+		});
+	});
+	// check connection
+	mqttclient.on("connect", function () {
+		console.log("Connected: ", mqttbroker, ":", mqttport);
+	});
+	mqttclient.on("error", function () {
+		console.log("An error occurred...");
+	});
+	// handle mqtt messages
+	mqttclient.on("message", function (topic, message) {
+		var topicArray = topic.split("/");
+		var payload = message.toString();
+		// don't handle messages with weird tokens, e.g. compression
+		try {
+			payload = JSON.parse(payload);
+		} catch (error) {
+			return;
+		}
+		switch (topicArray[1]) {
+		case "sensor":
+			handle_sensor(topicArray, payload);
+			break;
 
-          default:
-            break;
-        }
-        // emit received message to socketio listener
-        io.sockets.emit("mqtt", {
-            topic: topic,
-            payload: payload.toString()
-        });
-    });
-    // handle the sensor readings
-    function handle_sensor(topicArray, payload) {
-        switch (topicArray[3]) {
-          case "gauge":
-            var gauge = JSON.parse(payload);
-            // FLM gauges consist of timestamp, value, and unit
-            if (gauge.length == 2) {
-                // enhance payload w/o timestamp by current timestamp
-                var now = parseInt(new Date().getTime() / 1e3);
-                var new_payload = [];
-                new_payload.push(now, gauge[0], gauge[1]);
-                payload = JSON.stringify(new_payload);
-            }
-            // gauge length 2 is sent from Arduino sensors (in my case)
-            break;
+		default:
+			break;
+		}
+		// emit received message to socketio listener
+		io.sockets.emit("mqtt", {
+			topic : topic,
+			payload : JSON.stringify(payload)
+		});
+	});
+	// handle the sensor readings
+	function handle_sensor(topicArray, payload) {
+		switch (topicArray[3]) {
+		case "gauge":
+			// FLM gauges consist of timestamp, value, and unit
+			// gauge length 2 is sent from Arduino sensors (in my case)
+			switch (payload.length) {
+			case 2:
+				// enhance payload w/o timestamp by current timestamp
+				var now = parseInt(new Date().getTime() / 1e3);
+				payload.unshift(now);
+				break;
+			}
+			break;
 
-          default:
-            break;
-        }
-    }
+		default:
+			break;
+		}
+	}
 }
 
 // Serve the index.html page
 function httphandler(req, res) {
-    var uri = url.parse(req.url).pathname, filename = path.join(process.cwd(), uri);
-    var contentTypesByExtension = {
-        ".html": "text/html",
-        ".css": "text/css",
-        ".js": "text/javascript"
-    };
-    // serve requested files
-    fs.exists(filename, function(exists) {
-        if (!exists) {
-            res.writeHead(404, {
-                "Content-Type": "text/plain"
-            });
-            res.write("404 Not Found\n");
-            res.end();
-            return;
-        }
-        if (fs.statSync(filename).isDirectory()) filename += "/gauge.html";
-        fs.readFile(filename, "binary", function(err, file) {
-            if (err) {
-                res.writeHead(500, {
-                    "Content-Type": "text/plain"
-                });
-                res.write(err + "\n");
-                res.end();
-                return;
-            }
-            var headers = {};
-            var contentType = contentTypesByExtension[path.extname(filename)];
-            if (contentType) headers["Content-Type"] = contentType;
-            res.writeHead(200, headers);
-            res.write(file, "binary");
-            res.end();
-        });
-    });
+	var uri = url.parse(req.url).pathname,
+	filename = path.join(process.cwd(), uri);
+	var contentTypesByExtension = {
+		".html" : "text/html",
+		".css" : "text/css",
+		".js" : "text/javascript"
+	};
+	// serve requested files
+	fs.exists(filename, function (exists) {
+		if (!exists) {
+			res.writeHead(404, {
+				"Content-Type" : "text/plain"
+			});
+			res.write("404 Not Found\n");
+			res.end();
+			return;
+		}
+		if (fs.statSync(filename).isDirectory())
+			filename += "/gauge.html";
+		fs.readFile(filename, "binary", function (err, file) {
+			if (err) {
+				res.writeHead(500, {
+					"Content-Type" : "text/plain"
+				});
+				res.write(err + "\n");
+				res.end();
+				return;
+			}
+			var headers = {};
+			var contentType = contentTypesByExtension[path.extname(filename)];
+			if (contentType)
+				headers["Content-Type"] = contentType;
+			res.writeHead(200, headers);
+			res.write(file, "binary");
+			res.end();
+		});
+	});
 }
 
 mqttconnect();

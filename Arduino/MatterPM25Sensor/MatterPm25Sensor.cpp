5bb9c247015e3e87cb3a5e9d89e86c6d42ff20ce
Markus Gebhard <markus.gebhard@web.de>
2026-09-24 16:54:37 +0200

    fixing declaration error; use ESP32 4.0.0-rc1 or newer

--- Arduino/MatterPM25Sensor/MatterPm25Sensor.cpp
+++ Arduino/MatterPM25Sensor/MatterPm25Sensor.cpp
@@ -1,44 +1,16 @@
 #include <sdkconfig.h>
 #ifdef CONFIG_ESP_MATTER_ENABLE_DATA_MODEL
 
+// Uses matter implementation of ESP32 with at least release 4.0.0-rc1 to allow custom endpoints
 #include <Matter.h>
 #include "MatterPm25Sensor.h"
 
 using namespace esp_matter;
 using namespace esp_matter::endpoint;
 using namespace esp_matter::cluster;
-//using namespace chip::app::Clusters;
-
-// ---------------------------------------------------------------------------
-// Minimal callbacks required by node::create().
-// ArduinoMatter::_init() is protected (friend-class only), so we call
-// node::create() directly and own the node pointer ourselves.
-// ---------------------------------------------------------------------------
-static esp_err_t app_attribute_update_cb(
-  attribute::callback_type_t type, uint16_t endpoint_id,
-  uint32_t cluster_id, uint32_t attribute_id,
-  esp_matter_attr_val_t *val, void *priv_data) {
-  return ESP_OK;
-}
-
-static esp_err_t app_identification_cb(
-  identification::callback_type_t type, uint16_t endpoint_id,
-  uint8_t effect_id, uint8_t effect_variant, void *priv_data) {
-  return ESP_OK;
-}
-
-bool MatterPm25Sensor::attributeChangeCB(uint16_t endpoint_id, uint32_t cluster_id, uint32_t attribute_id, esp_matter_attr_val_t *val) {
-  bool ret = true;
-  if (!started) {
-    log_e("Matter PM2.5 Concentration Measurement Sensor device has not begun.");
-    return false;
-  }
-
-  log_d(
-    "PM2.5 Concentration Measurement Sensor Attr update callback: endpoint: %u, cluster: %" PRIu32 ", attribute: %" PRIu32 ", val: %" PRIu32, endpoint_id, cluster_id, attribute_id,
-    val->val.u32);
-  return ret;
-}
+using namespace esp_matter::cluster::concentration_measurement::feature;
+using namespace chip::app::Clusters;
+using namespace chip::app::Clusters::detail;
 
 MatterPm25Sensor::MatterPm25Sensor() {}
 
@@ -47,18 +19,7 @@ MatterPm25Sensor::~MatterPm25Sensor() {
 }
 
 bool MatterPm25Sensor::begin(int16_t _rawPm25Concentration) {
-  //ArduinoMatter::_init();
-  /*
-  node::config_t nodeConfig;
-  node_t *node = node::create(&nodeConfig, app_attribute_update_cb, app_identification_cb);
-  if (node == nullptr) {
-    log_e("Failed to create Matter node");
-    return false;
-  }
-
-  //ArduinoMatter::_init() would set the NodeCreated lifecycle indication
-  //sLifecycle = MatterLifecycle::NodeCreated;
-  */
+  // get endpoint to add own clusters to
   ensureMatterNode();
 
   if (getEndPointId() != 0) {
@@ -69,19 +30,16 @@ bool MatterPm25Sensor::begin(int16_t _rawPm25Concentration) {
   air_quality_sensor::config_t aqConfig;
 
   pm25_concentration_measurement::config_t pm25Config;
-  pm25Config.measurement_medium = 0;  // 0 = Air
-  // feature_flags: bit 0 = NumericMeasurement (MEA)
+  pm25Config.measurement_medium = chip::to_underlying(MeasurementMediumEnum::kAir);
   pm25Config.feature_flags =
     concentration_measurement::feature::numeric_measurement::get_id();
-
-  // Initial MeasuredValue: 0.0 µg/m³
-  pm25Config.features.numeric_measurement.measured_value = nullable<float>(0.0f);
+  pm25Config.features.numeric_measurement.measured_value = nullable<float>(_rawPm25Concentration);
   pm25Config.features.numeric_measurement.min_measured_value = nullable<float>();
   pm25Config.features.numeric_measurement.max_measured_value = nullable<float>();
-  pm25Config.features.numeric_measurement.measurement_unit = 1;  // 1 = µg/m³
+  pm25Config.features.numeric_measurement.measurement_unit = chip::to_underlying(MeasurementUnitEnum::kUgm3);
 
   // endpoint handles can be used to add/modify clusters
-  endpoint_t *endpoint = air_quality_sensor::create(node::get(), &aqConfig, ENDPOINT_FLAG_NONE, nullptr);
+  endpoint_t *endpoint = air_quality_sensor::create(node::get(), &aqConfig, ENDPOINT_FLAG_NONE, (void *)this);
   if (endpoint == nullptr) {
     log_e("Failed to create Air Quality Sensor endpoint.");
     return false;
--- Arduino/MatterPM25Sensor/MatterPm25Sensor.h
+++ Arduino/MatterPM25Sensor/MatterPm25Sensor.h
@@ -9,6 +9,16 @@ class MatterPm25Sensor : public MatterEndPoint {
 public:
   MatterPm25Sensor();
   ~MatterPm25Sensor();
+  // this function is called by Matter internal event processor. It could be overwritten by the application, if necessary.
+  bool attributeChangeCB(uint16_t endpoint_id, uint32_t cluster_id, uint32_t attribute_id, esp_matter_attr_val_t *val) override {
+    if (!started) {
+      log_e("Air Quality PM2.5 sensor has not begun.");
+      return false;
+    }
+    log_d(
+      "Attr update: endpoint %u, cluster 0x%08" PRIX32 ", attribute 0x%08" PRIX32, endpoint_id, cluster_id, attribute_id);
+    return true;
+  }
   // begin Matter PM2.5 Concentration Measurement Sensor endpoint with initial concentration
   bool begin(double ugPerM3 = 0.00) {
     return begin(static_cast<int16_t>(ugPerM3));
@@ -32,10 +42,17 @@ public:
   operator double() {
     return (double)getPm25Concentration();
   }
-  // this function is called by Matter internal event processor. It could be overwritten by the application, if necessary.
-  bool attributeChangeCB(uint16_t endpoint_id, uint32_t cluster_id, uint32_t attribute_id, esp_matter_attr_val_t *val);
 
 protected:
+  void onStackStarted() override {
+    esp_matter_attr_val_t val = esp_matter_nullable_float(nullable<float>(rawPm25Concentration));
+    lock::ScopedChipStackLock stackLock(portMAX_DELAY);
+    if (!updateAttributeVal(Pm25ConcentrationMeasurement::Id, Pm25ConcentrationMeasurement::Attributes::MeasuredValue::Id, &val)) {
+      log_e("Failed to apply cached PM2.5 value after Matter.begin().");
+    }
+  }
+
+private:
   bool started = false;
   int16_t rawPm25Concentration = 0;
   bool setRawPm25Concentration(int16_t _rawPm25Concentration);
